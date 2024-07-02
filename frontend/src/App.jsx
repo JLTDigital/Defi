605286c8c16cf74f2e4bd716382cad80a1fbfb16
JLTDigital <mrjltaft@gmail.com>
2024-07-02 19:33:21 +0100

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { VFXProvider } from 'react-vfx'
 import Header from './components/Header'
@@ -24,18 +24,20 @@ function App() {
         <Header />
         <main className='py-3'>
           <Container>
-            <Route path='/blockchain' component={BlockchainView} exact />
-            <Route path='/ethereum' component={EthereumView} exact />
-            <Route path='/dapps' component={DappsView} exact />
-            <Route path='/defi' component={DefiView} exact />
-            <Route path='/games' component={GamesView} exact />
-            <Route path='/nft' component={NFTView} exact />
-            <Route path='/developer' component={DeveloperView} exact />
-            <Route path='/exchanges' component={ExchangesView} exact />
-            <Route path='/links' component={LinksView} exact />
-            <Route path='/literature' component={LiteratureView} exact />
-            <Route path='/wallets' component={WalletView} exact />
-            <Route path='/' component={HomeView} exact />
+            <Routes>
+              <Route path='/blockchain' component={BlockchainView} exact />
+              <Route path='/ethereum' component={EthereumView} exact />
+              <Route path='/dapps' component={DappsView} exact />
+              <Route path='/defi' component={DefiView} exact />
+              <Route path='/games' component={GamesView} exact />
+              <Route path='/nft' component={NFTView} exact />
+              <Route path='/developer' component={DeveloperView} exact />
+              <Route path='/exchanges' component={ExchangesView} exact />
+              <Route path='/links' component={LinksView} exact />
+              <Route path='/literature' component={LiteratureView} exact />
+              <Route path='/wallets' component={WalletView} exact />
+              <Route path='/' component={HomeView} exact />
+            </Routes>
           </Container>
         </main>
         <Footer />
